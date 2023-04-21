b04550ef23a8429971852bc8d72295cb5dddee09
Giovanni Braconi <thumpback2002.it@gmail.com>
2023-04-21 10:30:45 +0200

    Adeed Dive and Rise Coroutines

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,28 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private Animator animator;
+    [SerializeField] private int diveForce = 5;
     private Rigidbody2D rigidbody2D;
+    private BoxCollider2D boxCollider2D;
     private Vector3 movementVector;
+    private AudioSource currentAudioSource;
     private bool facingRight = true;
     private bool isUnderwater = false;
+    private AudioManager audioManager;
 
     private void Awake()
     {
+        audioManager = AudioManager.GetInstance();
         isUnderwater = false;
         rigidbody2D = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
         movementVector = new Vector3();
     }
 
     void FixedUpdate()
     {
         movementVector.x = Input.GetAxis("Horizontal");
-        movementVector.y=0;
+        movementVector.y = 0;
         if (isUnderwater)
         {
             movementVector.y = Input.GetAxis("Vertical");
@@ -39,9 +45,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && !isUnderwater)
         {
             isUnderwater = true;
-            animator.SetBool("isUnderwater", true);
-            AudioManager.GetInstance().GetAudioSource("Ambience","SeaWaves").Stop();
-            AudioManager.GetInstance().GetAudioSource("Ambience","Abyss").Play();
+            StartCoroutine(Dive(1f));
         }
     }
 
@@ -57,8 +61,44 @@ public class PlayerController : MonoBehaviour
     {
         return movementVector.x > 0 && !facingRight || movementVector.x < 0 && facingRight;
     }
+
     public void SetIsUnderwater(bool isUnderwater)
     {
         this.isUnderwater = isUnderwater;
+    }
+
+    private IEnumerator SmoothLerp(float time)
+    {
+        Vector3 startingPos = gameObject.transform.position;
+        Vector3 finalPos = gameObject.transform.position + (transform.up * -2);
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            gameObject.transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator Dive(float time)
+    {
+        currentAudioSource = audioManager.GetAudioSource("SFX", "DeepBreath");
+        currentAudioSource.Play();
+        yield return WaitForSoundToFinish(currentAudioSource);
+        audioManager.GetAudioSource("SFX", "Bubbles").Play();
+        currentAudioSource = audioManager.GetAudioSource("Ambience", "SeaWaves");
+        yield return FadeAudioSource.StartFade(currentAudioSource, 0.3f, 0f);
+        currentAudioSource = audioManager.GetAudioSource("Ambience", "Abyss");
+        currentAudioSource.Play();
+        yield return FadeAudioSource.StartFade(currentAudioSource, 0.3f, 1f);
+        yield return SmoothLerp(time);
+        animator.SetBool("isUnderwater", true);
+        boxCollider2D.enabled = true;
+    }
+
+    private IEnumerator WaitForSoundToFinish(AudioSource audioSource)
+    {
+        yield return new WaitForSeconds(audioSource.clip.length);
     }
 }
--- Assets/Scripts/Surface.cs
+++ Assets/Scripts/Surface.cs
@@ -1,24 +1,36 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Surface : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private PlayerController playerController;
-
+    private AudioManager audioManager;
+    private AudioSource currentAudioSource;
     private void Awake()
     {
+        audioManager = AudioManager.GetInstance();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.name == "Player") ;
         {
-            Debug.Log("Player is on surface");
-            playerController.SetIsUnderwater(false);
-            animator.SetBool("isUnderwater", false);
-            AudioManager.GetInstance().GetAudioSource("Ambience", "Abyss").Stop();
-            AudioManager.GetInstance().GetAudioSource("Ambience", "SeaWaves").Play();
+            StartCoroutine(Rise(other));
         }
+    }
+
+    private IEnumerator Rise(Collision2D other)
+    {
+        Debug.Log("Player is on surface");
+        playerController.SetIsUnderwater(false);
+        animator.SetBool("isUnderwater", false);
+        other.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        currentAudioSource=audioManager.GetAudioSource("Ambience", "Abyss");
+        yield return FadeAudioSource.StartFade(currentAudioSource, 0.3f, 0f);
+        currentAudioSource = audioManager.GetAudioSource("Ambience", "SeaWaves");
+        yield return FadeAudioSource.StartFade(currentAudioSource, 0.3f, 1f);
+
     }
 }
